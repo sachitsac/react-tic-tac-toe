004a94caa342ca4b90d21eeab2a5c43829beb645
Sachit <sachitsac@gmail.com>
2021-05-28 23:55:08 +1000

    chore: added bootstrap and scss

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders Hi From my app", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hi From my app/i);
   expect(linkElement).toBeInTheDocument();
 });
